7b00f73c425ad597b0a0d2354708e8cfc2d92c99
sawyercole <sawyercoletang@gmail.com>
2022-11-30 15:25:26 -0500

    added collision boxes

--- src/ECS/RectangleColliderComponent.h
+++ src/ECS/RectangleColliderComponent.h
@@ -2,17 +2,22 @@
 #include <SDL.h>
 #include "AbsColliderComponent.h"
 
+//cleanup and add notes... add more robuestness to rects and stuff.
 class RectangleColliderComponent : public AbsColliderComponent {
 
 public:
     SDL_Rect collider;
-    SDL_Rect srcR, destR;
 
     /**
      *
      * @param t tag name of collider for debugging
      */
-    RectangleColliderComponent(std::string t) : AbsColliderComponent(t) {}
+    RectangleColliderComponent(std::string t) : AbsColliderComponent(t) {
+        collider.x = 0;
+        collider.y = 0;
+        collider.w = 0;
+        collider.h = 0;
+    }
 
     /**
      *
@@ -34,8 +39,8 @@ public:
         AbsColliderComponent::init();
 
         tex = TextureManager::LoadTexture("assets/ColTex.png");
-        srcR = { 0, 0, 32, 32 };
-        destR = {collider.x, collider.y, collider.w, collider.h};
+        AbsColliderComponent::srcR = { 0, 0, 32, 32 };
+        AbsColliderComponent::destR = {collider.x, collider.y, collider.w, collider.h};
     }
 
     /**
@@ -51,18 +56,37 @@ public:
             collider.h = transform->height * transform->scale;
         }
 
-//        if (tag == "worm" && colliding.count(Game::groupPlayers)) std::cout << "pog" << std::endl;
+            AbsColliderComponent::destR.x = collider.x - Game::camera.x;
+            AbsColliderComponent::destR.y = collider.y - Game::camera.y;
 
-        destR.x = collider.x - Game::camera.x;
-        destR.y = collider.y - Game::camera.y;
+
+    }
+
+    //fix later
+    SDL_Rect getColliderRect(){
+        SDL_Rect rect;
+        rect.x = collider.x;
+        rect.y = collider.y;
+        rect.w = collider.w;
+        rect.h = collider.h;
+        return rect;
     }
 
     /**
      * @brief draw the component and clear vector of currently colliding components.
+     *
+     * @todo change to draw Collider **not** destR
      */
     void draw() override {
         AbsColliderComponent::draw();
-        TextureManager::Draw(tex, srcR, destR, SDL_FLIP_NONE);
+//        TextureManager::Draw(tex, AbsColliderComponent::srcR, AbsColliderComponent::destR, SDL_FLIP_NONE);
+//        if (tag == "player") {
+            SDL_SetRenderDrawColor(Game::renderer, 255, 255, 255, 255);
+            //std::cout <<  destR.x << std::endl;
+            SDL_RenderDrawRect(Game::renderer, &(AbsColliderComponent::destR));
+//        }
+
+
     }
 
 };
